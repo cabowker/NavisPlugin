62b6103c67bf5c46ffba41c7c65258a1c6b51954
Chris Bowker <chris@valorVDC.com>
2023-07-16 16:04:53 -0400

    Initial commit

---
+++ NavisPlugin/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+using Autodesk.Navisworks.Api.Automation;
+
+namespace NavisPlugin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //when you add Navisworks Application it adds the nw.API.Automation
+            var nwApp = new NavisworksApplication();
+            nwApp.Visible = true;
+        }
+    }
+}
